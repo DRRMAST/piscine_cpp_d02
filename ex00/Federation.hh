514e686030fb57b633407c05b662c277dc4d2b2d
DRRMAST <heztzxy@yeah.net>
2018-07-03 16:53:03 +0800

    ex00

---
+++ ex00/Federation.hh
@@ -0,0 +1,42 @@
+#ifndef FEDERATION_HH
+#define FERERATION_HH
+
+#include <iostream>
+#include "Warpsystem.hh"
+
+namespace Federation
+{
+	namespace Starfleet
+	{
+		class Ship
+		{
+			public:
+				Ship(int length, int width, std::string name, short maxWarp);
+				~Ship();
+				void setupCore(WarpSystem::Core* ptr);
+				void checkCore();
+
+			private:
+				int _length;
+				int _width;
+				std::string _name;
+				short _maxWarp;
+			WarpSystem::Core* _core;
+		};
+	};
+	class Ship
+	{
+		public:
+			Ship(int length, int width, std::string name);
+			~Ship();
+			void setupCore(WarpSystem::Core* ptr);
+			void checkCore();
+		private:
+		int _length;
+		int	_width;
+		std::string	_name;
+		short _maxWarp;
+		WarpSystem::Core* _core;
+	};
+};
+#endif	
